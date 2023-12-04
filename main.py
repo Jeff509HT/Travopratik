f4d92d5eead381c16fd3a8b155ff2db46d9945de
Jeff898 <90300775+Jeff509HT@users.noreply.github.com>
2023-12-04 07:44:54 -0800

    Update modil

---
+++ main.py
@@ -0,0 +1,11 @@
+import dev
+
+example = "Ayiti kapab avanse"
+print(miniskil_chenn(example))
+print(majiskil_premye_let(example))
+print(nouvo_chenn_ak_inisyal(example))
+print(ranplase_a_avek_at(example))
+print(endeks_premye_a(example))
+print(total_endeks_a(example))
+print(lis_endeks_miniskil_a(example))
+print(retire_espas_kole_kantite(example))
